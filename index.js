7471d7faa3dc36dded3d8c54f3524e8213f0160c
Akil Brooks <akil.brooks@gmail.com>
2020-05-07 18:29:31 -0400

    feat(rules): Expand rules for scope and subject

--- index.js
+++ index.js
@@ -4,11 +4,14 @@ module.exports = {
     "body-max-line-length": [2, "always", 72],
     "footer-leading-blank": [2, "always"],
     "header-max-length": [2, "always", 50],
-    "scope-case": [2, "always", "lower-case"],
+    "scope-case": [2,
+      "always",
+      ["lower-case", "camel-case", "pascal-case"]
+    ],
     "subject-case": [
       2,
       "never",
-      ["sentence-case", "start-case", "pascal-case", "upper-case"]
+      ["start-case", "pascal-case", "upper-case"]
     ],
     "subject-empty": [2, "never"],
     "subject-full-stop": [2, "never", "."],
@@ -25,6 +28,7 @@ module.exports = {
         "feat",
         "fix",
         "perf",
+        "improvement",
         "refactor",
         "revert",
         "style",
